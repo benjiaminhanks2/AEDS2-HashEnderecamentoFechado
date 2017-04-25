e7ebfb1cd534428fe3a9eb90155dd75cde9eb461
ruanformigoni <formigoni@outlook.com>
2017-04-25 11:13:08 -0300

    Funcao "peso" Atualizada

--- hash.h
+++ hash.h
@@ -19,7 +19,7 @@ typedef struct{
 void add(Table *t, Chave *c, int tam);
 void transf(Table *t, Chave c);
 void antiColisoes(Item *i,Chave c);
-int peso(int i);
+unsigned int peso(unsigned int i, Table *t);
 int calcM(int n);
 void initHash(Table *t,int M);
 void print(Table *t);
